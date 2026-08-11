edcef70e0521e9a6145634a8eb4b110510490525
Akshat Lakhotiya <akshat.lakhotiya_cs.aiml24@gla.ac.in>
2026-08-11 22:58:28 +0530

    Time: 824 ms (5.01%), Space: 42.1 MB (42.93%) - LeetHub

---
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int countOdds(int low, int high) {
+        int count=0;
+        if(low%2==0){
+            low++;
+        }
+        for(int i=low;i<=high;i+=2){
+            count++;
+
+        }
+        return count;
+        
+    }
+}
