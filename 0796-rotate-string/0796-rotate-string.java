8151dd1e1247ca9938f09d0d7b9866869936b3cc
Akshat Lakhotiya <akshat.lakhotiya_cs.aiml24@gla.ac.in>
2026-08-16 19:27:51 +0530

    Time: 2 ms (58.98%), Space: 43.5 MB (45.64%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if(s.length()!=goal.length()){
+            return false;
+        }
+        else if((s+s).contains(goal)){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+
+        
+    }
+}
