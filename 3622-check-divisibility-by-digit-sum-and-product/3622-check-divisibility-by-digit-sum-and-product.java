95514f08e50a1379a02a352d2d073d1ec00a757b
Akshat Lakhotiya <akshat.lakhotiya_cs.aiml24@gla.ac.in>
2026-08-23 21:43:05 +0530

    Time: 0 ms (100%), Space: 42.6 MB (24.43%) - LeetHub

---
+++ 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean checkDivisibility(int n) {
+        int sum=0;
+        int product=1;
+        int temp=n;
+        while(temp>0){
+            int digit=temp%10;
+            sum+=digit;
+            product*=digit;
+            temp=temp/10;
+        }
+        if(n%(sum+product)==0){
+            return true;
+        }
+        return false;
+        
+    }
+}
