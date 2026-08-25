d60addb74c22916614a52acf23be1db717cb0405
Akshat Lakhotiya <akshat.lakhotiya_cs.aiml24@gla.ac.in>
2026-08-25 21:59:22 +0530

    Time: 0 ms (100%), Space: 43.4 MB (59.83%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,28 +1,6 @@
 class Solution {
     public void sortColors(int[] nums) {
-        /*int n=nums.length;
-        int z=0;
-        int t=n-1;
-        int i=0;
-        while(i<=t){
-            if(nums[i]==0){
-                int temp=nums[i];
-                nums[i]=nums[z];
-                nums[z]=temp;
-                z++;
-                i++;
-            }
-            else if(nums[i]==2){
-                int temp=nums[i];
-                nums[i]=nums[t];
-                nums[t]=temp;
-                t--;
-            }
-            else{
-                
-            }
-
-        }*/
+        /*
         int count[]=new int[3];
         for(int num:nums){
             count[num]++;
@@ -33,6 +11,28 @@ class Solution {
                 nums[index]=i;
                 index++;
                 count[i]--;
+            }
+        }*/
+
+        int z=0;
+        int i=0;
+        int t=nums.length-1;
+        while(i<=t){
+            if(nums[i]==0){
+                int temp=nums[i];
+                nums[i]=nums[z];
+                nums[z]=temp;
+                i++;
+                z++;
+            }
+            else if(nums[i]==2){
+                int temp=nums[i];
+                nums[i]=nums[t];
+                nums[t]=temp;
+                t--;
+            }
+            else{
+                i++;
             }
         }
   
